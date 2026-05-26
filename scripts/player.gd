ee2d76e4a058a24b2d60e6d417c15310f7119ce5
Gucks <justahiru@gmail.com>
2026-05-26 10:08:51 +0700

    Implement tweening into acceleration for jumping and running.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,62 @@
 extends CharacterBody2D
 
 const MAX_SPEED = 150
-const JUMP_VELOCITY = -300
 const ACCELERATION = 500
 const DECELERATION = 600
+const MAX_JUMP_VELOCITY = -300
+const TIME_TO_REACH_MAX_JUMP = .5
 
-@onready var accelerationTween = get_tree().create_tween()
+var jump_timer := 0.0
+var jump_velocity := 0.0
+
+@onready var acceleration_tween = get_tree().create_tween()
+@onready var jump_accel_tween = get_tree().create_tween()
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func setVelocityX(value):
-	velocity.x = value
+enum States {
+	ON_FLOOR,
+	JUMPING,
+	FALLING
+}
+var current_state: States = States.FALLING: set = set_state
+
+
+func jump_process(delta: float):
+	print(States.find_key(current_state))
+	match current_state:
+		States.ON_FLOOR:
+			if Input.is_action_just_pressed("jump"):
+				jump_accel_tween = create_tween()
+				jump_accel_tween.tween_method(set_velocityY, 0, MAX_JUMP_VELOCITY, TIME_TO_REACH_MAX_JUMP).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
+				set_state(States.JUMPING)
+				return
+			jump_timer = 0
+
+		States.FALLING:
+			if is_on_floor():
+				velocity.y = 0
+				set_state(States.ON_FLOOR)
+				return
+			velocity += get_gravity() * delta
+
+		States.JUMPING:
+			if jump_timer >= TIME_TO_REACH_MAX_JUMP or Input.is_action_just_released("jump"):
+				if jump_accel_tween:
+					jump_accel_tween.kill()
+
+				set_state(States.FALLING)
+				return
+			jump_timer += delta
+
 
 func _physics_process(delta: float) -> void:
-		# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	jump_process(delta)
 
 	var direction := Input.get_axis("left", "right")
 	
 	# Hanlde animation.
 	if not is_on_floor():
 		# Add the gravity.
-		velocity += get_gravity() * delta
 		animated_sprite.play("jump")
 	else:
 		# Play idle animation if standing still, else play "run" animation.
@@ -32,21 +67,33 @@ func _physics_process(delta: float) -> void:
 
 	# Handle sprite direction and velocity.
 	if direction:
-		if accelerationTween:
-			accelerationTween.kill()
+		if acceleration_tween:
+			acceleration_tween.kill()
 
-		accelerationTween = create_tween()
-		accelerationTween.tween_method(setVelocityX, velocity.x, direction * MAX_SPEED, 1).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+		acceleration_tween = create_tween()
+		acceleration_tween.tween_method(set_velocityX, velocity.x, direction * MAX_SPEED, 1.5).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 
 		if direction > 0:
 			animated_sprite.flip_h = false
 		else:
 			animated_sprite.flip_h = true
 	else:
-		if accelerationTween:
-			accelerationTween.kill()
+		if acceleration_tween:
+			acceleration_tween.kill()
 
-		accelerationTween = create_tween()
-		accelerationTween.tween_method(setVelocityX, velocity.x, 0, 1).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+		acceleration_tween = create_tween()
+		acceleration_tween.tween_method(set_velocityX, velocity.x, 0, 1).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 		
 	move_and_slide()
+
+
+func set_state(new_state: States):
+	current_state = new_state
+
+
+func set_velocityX(value: float):
+	velocity.x = value
+
+
+func set_velocityY(value: float):
+	velocity.y = value
